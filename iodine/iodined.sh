2e06bb80e46c2649399360618c554faa70d24d21
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2015-05-18 21:36:21 +0000

    modificata per funzionare su rapsbery

--- iodine/iodined.sh
+++ iodine/iodined.sh
@@ -4,4 +4,4 @@
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 
-exec iodined -c -f 10.16.0.1 $IODINE_HOST -P $IODINE_PASSWORD >>/var/log/iodined.log 2>&1
+exec iodined -c -f -p 53 10.16.0.1 -P $IODINE_PASSWORD $IODINE_HOST
